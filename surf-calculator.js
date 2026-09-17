07674a09d17e9389a419b0a389c8a669874906ee
acarolmourad-commits <acarol.mourad@gmail.com>
2026-09-16 21:30:50 -0300

    feat: add Mercado, Mapa and Maré to satellite nav (26 sites)

--- surf-calculator.js
+++ surf-calculator.js
@@ -111,7 +111,7 @@ window.SurfCalculator = SurfCalculator;
     ['Nutrição','asf-nutricao'],['Viagens','asf-viagens'],['História','asf-historia'],
     ['Bem-estar','asf-bemestar'],['Diário','asf-diario'],['Eco','asf-eco'],
     ['Mental','asf-mental'],['Kids','asf-kids'],['SOS','asf-sos'],
-    ['Yoga','asf-yoga'],['Volume','asf-volume'],['Respira','asf-respira'],['Ranking','asf-ranking']
+    ['Yoga','asf-yoga'],['Volume','asf-volume'],['Respira','asf-respira'],['Ranking','asf-ranking'],['Mercado','asf-mercado'],['Mapa','asf-mapa'],['Maré','asf-mare']
   ];
   var btn = document.createElement('button');
   btn.id = 'asf-sat-nav';
